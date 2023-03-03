6f4eaa862541aec3744eadfcbeb7a170abdf6001
William Kieffer <william.kieffer@outlook.com>
2023-03-02 19:13:43 -0500

    Add detail and CSS

---
+++ index.js
@@ -0,0 +1,5 @@
+function getModel() {
+    const file = new File(["testing!"], "functionText.txt");
+    document.getElementById("tester").innerHTML = "IT worlks!"
+    //document.getElementById("functionInput").value
+}
