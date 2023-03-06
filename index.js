a2b25005f97941965189800ebfbe3f1f71111cf9
William Kieffer <william.kieffer@outlook.com>
2023-03-06 14:06:29 -0500

    Add pages, add content

--- index.js
+++
@@ -1,5 +0,0 @@
-function getModel() {
-    const file = new File(["testing!"], "functionText.txt");
-    document.getElementById("tester").innerHTML = "IT worlks!"
-    //document.getElementById("functionInput").value
-}
---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#header").html(`
+    <h1 id="header-name">William Kieffer</h1>
+    <div id="navbar">
+        <ul>
+            <li class="nav"><a class="navlink" href="index.html">Home</a></li>
+            <li class="nav"><a class="navlink" href="about.html">About</a></li>
+            <li class="nav"><a class="navlink" href="personal.html">Personal</a></li>
+            <li class="nav"><a class="navlink" href="links.html">Links</a></li>
+            <li class="nav"><a class="navlink" href="contact.html">Contact</a></li>
+        </ul>
+    </div>
+    `);
+    $("#footer").html(`
+    <p>Last Updated: March 2023</p>
+    `);
+});
