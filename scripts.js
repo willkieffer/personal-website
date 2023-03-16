554a9b539051e1c5c6601c8256d33597c287764d
William Kieffer <william.kieffer@outlook.com>
2023-03-16 16:45:57 -0400

    Update CSS files

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     <h1 id="header-name">William Kieffer</h1>
     <div id="navbar">
         <ul>
-            <li class="nav"><a class="navlink" href="index.html">Home</a></li>
-            <li class="nav"><a class="navlink" href="about.html">About</a></li>
-            <li class="nav"><a class="navlink" href="personal.html">Personal</a></li>
-            <li class="nav"><a class="navlink" href="links.html">Links</a></li>
-            <li class="nav"><a class="navlink" href="connect.html">Connect</a></li>
+            <li class="nav grow"><a class="navlink" href="index.html">Home</a></li>
+            <li class="nav grow"><a class="navlink" href="about.html">About</a></li>
+            <li class="nav grow"><a class="navlink" href="personal.html">Personal</a></li>
+            <li class="nav grow"><a class="navlink" href="links.html">Links</a></li>
+            <li class="nav grow"><a class="navlink" href="connect.html">Connect</a></li>
         </ul>
     </div>
     `);
